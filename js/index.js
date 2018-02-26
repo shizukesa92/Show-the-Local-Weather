bb7031959237f4239b0abea2730042e38ae88566
Leonard Tye <leonardtye1992@gmail.com>
2018-02-27 01:56:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $.simpleWeather ({
     success: function(weather) {
       $("#country").html(weather.city+", "+weather.region);             $("#temperature").html(weather.temp+"&deg;"+weather.units.temp);
       $("#weather").html(weather.currently);
-      $("#icon").html(weather.image);
+      $("#icon").html('"<img src="'+weather.image+'"/>');
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
